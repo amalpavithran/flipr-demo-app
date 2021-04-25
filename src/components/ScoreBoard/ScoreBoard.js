9afd910fb11e511c7d08e1e4ff007d2389639a22
aswinunni01 <aswinunni01@gmail.com>
2021-04-25 21:49:57 +0530

    more checks

--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -3,6 +3,7 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 import './ScoreBoard.css'
 class ScoreBoard extends React.Component{
     constructor(props){
